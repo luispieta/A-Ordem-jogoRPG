9ef122a566bd0290b7a9446a87761b291a9c6d1d
luispieta <lufelipepieta@gmail.com>
2025-06-10 22:11:58 -0300

    feat: Criação/Adição de outros documentos e códigos

---
+++ src/main/java/a/ordem/jogo/ApiAOrdemApplication.java
@@ -0,0 +1,13 @@
+package a.ordem.jogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAOrdemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAOrdemApplication.class, args);
+	}
+
+	}
---
+++ src/test/java/a/ordem/jogo/ApiAOrdemApplicationTests.java
@@ -0,0 +1,13 @@
+package a.ordem.jogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAOrdemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
